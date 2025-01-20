932d580790ef82bd704687997a2ed28f6eb3c429
mariamtarek <mariam.tarek@oneorder.net>
2025-01-20 16:11:16 +0200

    feat: intial app

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+
+<template>
+  <div id="app" class="text-center font-bold">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default{
+  name: 'App',
+}
+</script>
---
+++ src/router/routes.js
@@ -0,0 +1,21 @@
+ const routes = [
+    {
+      path: '/',
+      name: 'Layout',
+      component: ()=>import('../Layout/index.vue'),
+      children:[
+        {
+            name:"home",
+            path:"/",
+            component:()=>import('../pages/Home/index.vue'),
+        },
+        {
+            name:"Login",
+            path:"/login",
+            component:()=>import('../pages/Login/index.vue'),
+          },
+      ]
+    },
+    
+  ]
+  export default routes
