5f59ef5334e99fc8b7fc74be97f4808bacac9380
mariamtarek <mariam.tarek@oneorder.net>
2025-01-21 13:04:01 +0200

    feat: let app use fontawesome icons

--- src/main.js
+++ src/main.js
@@ -3,8 +3,14 @@ import App from './App.vue'
 import './assets/tailwind.css';
 import store from './store'
 import router from './router';
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { faEye, faEyeSlash} from '@fortawesome/free-solid-svg-icons'
+
+library.add(faEye, faEyeSlash, faCoffee)
 
 const app=createApp(App)
 app.use(store)
 app.use(router)
+app.component('font-awesome-icon', FontAwesomeIcon)
 app.mount('#app')
