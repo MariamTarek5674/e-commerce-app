ae710cac6722f41d38eee3b5ad8e153844f09f59
mariamtarek <mariam.tarek@oneorder.net>
2025-01-21 15:40:39 +0200

    feat: added navigation guard

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,21 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import store from '../store'
 import routes from './routes.js'
 const router = createRouter({
   history: createWebHistory(),
   routes
 })
+
+router.beforeEach((to, from, next) => {
+    if(store.getters['auth/isAuthenticated']){
+      next()
+    }else{
+      if (to.path === '/login') {
+        next();
+      } else {
+        next({ path: '/login' });
+      }
+    }
+ })
 
 export default router
