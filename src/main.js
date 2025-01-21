49acad0f602fe2b29139e3ab9102bbc15e88d3a8
mariamtarek <mariam.tarek@oneorder.net>
2025-01-21 15:18:07 +0200

    feat: handle app to use toaster

--- src/main.js
+++ src/main.js
@@ -6,11 +6,15 @@ import router from './router';
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import { faEye, faEyeSlash} from '@fortawesome/free-solid-svg-icons'
+import { createToaster } from '@bastien-j/vue-toaster'
+import '@bastien-j/vue-toaster/style.css' 
 
 library.add(faEye, faEyeSlash)
+const toaster = createToaster()
 
 const app=createApp(App)
 app.use(store)
 app.use(router)
+app.use(toaster)
 app.component('font-awesome-icon', FontAwesomeIcon)
 app.mount('#app')
