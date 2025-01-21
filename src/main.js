0fe7daf281b08c90a40e054ba2597bdf00bf6d57
mariamtarek <mariam.tarek@oneorder.net>
2025-01-21 16:23:44 +0200

    feat: cart icon added

--- src/main.js
+++ src/main.js
@@ -5,11 +5,11 @@ import store from './store'
 import router from './router';
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-import { faEye, faEyeSlash} from '@fortawesome/free-solid-svg-icons'
+import { faEye, faEyeSlash,faShoppingCart} from '@fortawesome/free-solid-svg-icons'
 import { createToaster } from '@bastien-j/vue-toaster'
 import '@bastien-j/vue-toaster/style.css' 
 
-library.add(faEye, faEyeSlash)
+library.add(faEye, faEyeSlash,faShoppingCart)
 const toaster = createToaster()
 
 const app=createApp(App)
