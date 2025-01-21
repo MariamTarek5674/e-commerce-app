e812013284bc624b6d3b19a4a9a9e0705853fa48
mariamtarek <mariam.tarek@oneorder.net>
2025-01-21 13:05:06 +0200

    fix: undefined icon

--- src/main.js
+++ src/main.js
@@ -7,7 +7,7 @@ import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import { faEye, faEyeSlash} from '@fortawesome/free-solid-svg-icons'
 
-library.add(faEye, faEyeSlash, faCoffee)
+library.add(faEye, faEyeSlash)
 
 const app=createApp(App)
 app.use(store)
