be16f028637672def70b5c51571a86f0e41b33ae
mariamtarek <mariam.tarek@oneorder.net>
2025-01-22 15:24:56 +0200

    feat: handled cart status toggle when the cart icon ic clicked

--- src/components/NavigationBar/index.vue
+++ src/components/NavigationBar/index.vue
@@ -7,7 +7,7 @@
      </div>
      <div class="flex gap-8 items-center">
       <div>
-        <i class="font-bold text-2xl cursor-pointer relative" >
+        <i class="font-bold text-2xl cursor-pointer relative" @click="changeCartStatus" >
          <font-awesome-icon icon="shopping-cart" class="icon text-black" />
          <span class="absolute top-0 -mt-5 right-0 -me-4 inline-flex items-center justify-center py-[0.2rem] px-[0.5rem] text-sm text-black border-black font-extrabold"> 3 </span>
        </i>
@@ -25,6 +25,9 @@ export default{
     handleLogout(){
       this.$store.dispatch('auth/logout')
       this.$router.push('/login')
+    },
+    changeCartStatus(){
+      this.$store.commit('cart/toggleCartStatus')
     }
    }
 }
