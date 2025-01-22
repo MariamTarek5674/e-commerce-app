58d393732c9818bb1ad3ff17d9bb50fb69424dab
mariamtarek <mariam.tarek@oneorder.net>
2025-01-22 14:00:40 +0200

    feat: skeleton component

---
+++ src/components/Skeleton.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class=" bg-gray-200 rounded-md animate-pulse"></div>
+</template>
+
+<script>
+export default{
+    name:'Skeleton'
+}
+</script>
+
+<style scoped>
+@keyframes pulse {
+    0% {
+        background-color: #e0e0e0;
+    }
+    50% {
+        background-color: #f0f0f0;
+    }
+    100% {
+        background-color: #e0e0e0;
+    }
+}
+
+.animate-pulse {
+    animation: pulse 1.5s infinite ease-in-out;
+}
+</style>
