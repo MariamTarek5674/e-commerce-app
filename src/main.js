9868ca573ba43dea8065b73e54b6ff3c0e548140
mariamtarek <mariam.tarek@oneorder.net>
2025-01-23 10:55:09 +0200

    feat: added used icons

--- src/main.js
+++ src/main.js
@@ -5,11 +5,11 @@ import store from './store'
 import router from './router';
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-import { faEye, faEyeSlash,faShoppingCart} from '@fortawesome/free-solid-svg-icons'
+import { faEye, faEyeSlash,faShoppingCart,faTimes,faTrash} from '@fortawesome/free-solid-svg-icons'
 import { createToaster } from '@bastien-j/vue-toaster'
 import '@bastien-j/vue-toaster/style.css' 
 
-library.add(faEye, faEyeSlash,faShoppingCart)
+library.add(faEye, faEyeSlash,faShoppingCart,faTimes,faTrash)
 const toaster = createToaster()
 
 const app=createApp(App)
