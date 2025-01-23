16e48921e7026da3a7c340d9976d6930d76a1be7
mariamtarek <mariam.tarek@oneorder.net>
2025-01-23 14:03:44 +0200

    feat: line item card skeleton

---
+++ src/components/Cart/components/lineItemCardSekleton.vue
@@ -0,0 +1,26 @@
+<template>
+ <div class="flex justify-between h-[100px]">
+    <div class="flex w-full h-full gap-2">
+        <skeleton class="h-full w-1/4"/>
+        <div class="flex gap-2 w-2/3 flex-col justify-center items-start whitespace-nowrap">
+            <skeleton class="h-6 w-3/4"/>
+             <skeleton class="h-6 w-1/2"/>
+        </div>
+    </div>
+    <div class='w-1/4 pt-2 flex justify-between items-center'>
+       <skeleton class="h-4 w-4"/>
+       <skeleton class="h-4 w-4"/>
+       <skeleton class="h-4 w-4"/>
+    </div>
+ </div>
+</template>
+
+<script>
+import Skeleton from '../../Skeleton.vue';
+export default{
+    name:'lineItemCardSeleton',
+    components:{
+        Skeleton
+    }
+}
+</script>
