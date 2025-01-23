dc4bfc91b8c5e886e6d4b28b03105775ee2592e9
mariamtarek <mariam.tarek@oneorder.net>
2025-01-23 14:03:25 +0200

    feat: line item card

--- src/components/Cart/components/lineItemCard.vue
+++ src/components/Cart/components/lineItemCard.vue
@@ -1,47 +1,57 @@
 <template>
-    <div class="flex justify-between h-[100px] ">
-                  <div class="flex w-1/4 h-full gap-2">
-                    <img class="h-full object-contain"
-                      :src="cardData.image"
-                      alt=""
-                    />
-                    <div class="flex flex-col justify-center items-start whitespace-nowrap">
-                      <div class="text-left">
-                        {{ truncatedTitle(cardData.title)   }}
-                      </div>
-                      <div class="font-bold text-lg p-2">{{cardData.price}} EGP</div>
-                     
-                    </div>
-                  </div>
-                  <div
-                    class='w-1/4 pt-2 flex justify-between items-center'
-                  >
-                    <button
-                      class='w-[25px] h-[25px] border-black border-2 font-bold cursor-pointer flex items-center  justify-center p-0 m-0'
-                      >
-                       -
-                    </button>
-                    <span class="font-bold text-black">{{cardData.quantity}}</span>
-                    <button
-                      class="w-[25px] h-[25px] border-black border-2 font-bold cursor-pointer flex items-center  justify-center p-0 m-0 "
-                    >
-                      +
-                    </button>
-                  </div>
+    <div v-if="!loading" class="flex justify-between h-[100px] ">
+         <div class="flex w-1/4 h-full gap-2">
+            <img class="h-full object-contain" :src="product.image" alt="" />
+            <div class="flex flex-col justify-center items-start whitespace-nowrap">
+                <div class="text-left">
+                        {{ truncatedTitle(product.title)   }}
                 </div>
+                <div class="font-bold text-lg p-2">{{product.price}} EGP</div>
+                     
+             </div>
+        </div>
+        <div class='w-1/4 pt-2 flex justify-between items-center'>
+            <button class='w-[25px] h-[25px] border-black border-2 font-bold cursor-pointer flex items-center  justify-center p-0 m-0'>
+                       -
+            </button>
+            <span class="font-bold text-black">{{productData.quantity}}</span>
+            <button class="w-[25px] h-[25px] border-black border-2 font-bold cursor-pointer flex items-center  justify-center p-0 m-0 ">
+                      +
+            </button>
+        </div>
+    </div>
+    <line-item-card-skeleton v-else />
 </template>
 <script>
+import lineItemCardSkeleton from './lineItemCardSekleton.vue'
 export default{
     name:'lineItemCard',
+    components:{
+        lineItemCardSkeleton
+    },
     props:{
-        cardData:{
+        productData:{
             type:Object,
             Required:true,
         }
     },
+    data(){
+        return{
+            product:{},
+            loading:true,
+        }
+    },
+    mounted(){
+        this.fetchProduct()
+    },
     methods: {
     truncatedTitle(title) {
-      return title.length > 20 ? title.slice(0, 20) + '...' : title;
+      return title?.length > 20 ? title.slice(0, 20) + '...' : title;
+    },
+    async fetchProduct(){
+       let response= await this.$store.dispatch('home/fetchProductDetails',this.productData.productId)
+       this.product=response.data
+       this.loading=false
     }
 },
 
