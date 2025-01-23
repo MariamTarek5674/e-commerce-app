d0b0ae774de9a9dcea558661057aa3e8408311c6
mariamtarek <mariam.tarek@oneorder.net>
2025-01-23 10:57:05 +0200

    feat: handled cart being viewed in layout component

--- src/Layout/index.vue
+++ src/Layout/index.vue
@@ -1,14 +1,22 @@
 <template>
     <Navigation-bar/>
+    <cart v-if="isCartOpen"/>
     <router-view/>
 </template>
 
 <script >
 import NavigationBar from '../components/NavigationBar/index.vue'
+import Cart from '../components/Cart/index.vue'
 export default{
     name:"LayoutComponent",
     components:{
-        NavigationBar
+        NavigationBar,
+        Cart
+    },
+    computed:{
+        isCartOpen(){
+            return this.$store.getters['cart/isCartOpen']
+        }
     }
 }
 </script>
