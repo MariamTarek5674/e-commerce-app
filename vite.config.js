79074f896ebb3dbee5ffecbdbca6126614c9b325
mariamtarek <mariam.tarek@oneorder.net>
2025-01-26 13:11:54 +0200

    feat: vite configuration

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,12 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  test: {
+    globals: true,        // Enable global test API like `describe`, `it`, etc.
+    environment: 'jsdom', // Use jsdom for DOM-like environment
+    setupFiles: './src/test/setup.js', // Optional: Setup file for global configurations
+  },
+  coverage: {
+    reporter: ['text', 'json', 'html'],
+  },
 })
