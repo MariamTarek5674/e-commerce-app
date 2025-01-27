0a48dc61ba2cab5630c0c94e05666b4a28b98c96
mariamtarek <mariam.tarek@oneorder.net>
2025-01-27 12:02:55 +0200

    feat: added cart and home store and productDetails route

--- src/test/setup.js
+++ src/test/setup.js
@@ -1,5 +1,7 @@
 import { createStore } from 'vuex';
 import { createRouter, createWebHistory } from 'vue-router';
+import Home from '../pages/Home/index.vue'
+import ProductDetails from '../pages/ProductDetails/index.vue'
 
 let store, router;
 beforeAll(() => {
@@ -8,15 +10,39 @@ beforeAll(() => {
         auth: {
           namespaced: true,
           actions:{
-            login: vi.fn(() => Promise.resolve()) //mock successful login action
+            login: vi.fn(() => Promise.resolve()), //mock successful login action
+            logout: vi.fn(()=>Promise.resolve()) //mock successful logout action
           }
         },
-      },
-    });
-    store.dispatch = vi.fn(); // mock the dispatch function itself not only the login action so i can spy on login function (because the actions is wrapped with another BoundDispatch)
+        home: {
+            namespaced: true,
+            actions: {
+              fetchProductDetails: vi.fn(() => {
+                return Promise.resolve({ data: { title: "Test Product", price: 100, image: "test-image.jpg" } });
+              }),
+            },
+          },
+          cart: {
+            namespaced: true,
+            state:{
+                isCartOpen:false
+            },
+            actions: {
+              updateProductQuantity: vi.fn(() => Promise.resolve()),
+            },
+            getters: {
+              userCarts: () => [{ id: 1 }],
+              cartItemsLength: ()=> 4
+            },
+            mutations:{
+                toggleCartStatus: (state)=> state.isCartOpen = !state.isCartOpen
+            }
+          },
+}
+})
     router = createRouter({
       history: createWebHistory(),
-      routes: [{ path: '/', name: 'Home', component: { template: '<div>Home</div>' } }],
+      routes: [{ path: '/', name: 'Home', component: Home, children:[{path:'product/:id',name:'productDetails',component:ProductDetails}] }],
     });
 
 globalThis.store= store;
