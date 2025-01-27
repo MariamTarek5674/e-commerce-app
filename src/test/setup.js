e47815c0e7c9f90ebd18dc0c939ca9ca7b0d5a7c
mariamtarek <mariam.tarek@oneorder.net>
2025-01-27 14:35:16 +0200

    feat: updated cart module store

--- src/test/setup.js
+++ src/test/setup.js
@@ -21,22 +21,58 @@ beforeAll(() => {
                 return Promise.resolve({ data: { title: "Test Product", price: 100, image: "test-image.jpg" } });
               }),
             },
+            getters:{
+                getProductById: vi.fn((state)=>(productId)=>{
+                    return { title: "Test Product", price: 100, image: "test-image.jpg"}
+                  })
+            }
           },
           cart: {
             namespaced: true,
             state:{
-                isCartOpen:false
+                isCartOpen:false,
+                fetchCartLoading:false,
+                userCarts:[],
+                cartItemsLength:0,
             },
             actions: {
               updateProductQuantity: vi.fn(() => Promise.resolve()),
+              fetchUserCarts: vi.fn(async ({ commit }) => {
+                const response = {
+                  data: [
+                    {
+                      id: 1,
+                      products: [
+                        { id: 1, productId: 1, quantity: 2 },
+                        { id: 2, productId: 2, quantity: 1 },
+                      ],
+                    },
+                  ],
+                };
+                commit('setFetchCartLoading', true); // Start loading
+                commit('setUserCarts', [response.data[0]]);
+                
+                let length = 0;
+                response.data.forEach((cart) => {
+                  length += cart?.products?.length;
+                });
+    
+                //commit('setCartItemsLength', length); // Set cart items length
+                commit('setFetchCartLoading', false); // Stop loading
+              }),
+              clearCart:({state}) => state.userCarts=[]
             },
             getters: {
-              userCarts: () => [{ id: 1 }],
-              cartItemsLength: ()=> 4
+              cartItemsLength: ()=> 4,
+              userCarts:(state)=> state.userCarts,
+              fetchCartLoading:(state)=> state.fetchCartLoading,
+              cartId:()=>1
             },
             mutations:{
-                toggleCartStatus: (state)=> state.isCartOpen = !state.isCartOpen
-            }
+                toggleCartStatus: (state)=> state.isCartOpen = !state.isCartOpen,
+                setUserCarts :(state,items)=>  state.userCarts=items,
+                setFetchCartLoading:(state,status)=> state.fetchCartLoading=status,
+            }   
           },
 }
 })
